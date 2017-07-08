5606462ae4150adcd890e6d258174ca1afb5f7bd
mrNoBoDy <mrnobody1042@ya.ru>
2017-07-08 14:42:53 +0300

    Сделано: Поиск лучшего студента, Обработчик команд пользователя, Оптимизация кода, Избавление от повторений кода, Добавление комментариев

--- Homework 1.5.py
+++ Homework 1.5.py
@@ -6,17 +6,11 @@
 #   Необходимые команды:
 # 1) Средняя оценка за домашние задания и экзамен по всей группе;
 # 2) Средняя оценка по параметрам, которые были выбраны пользователем
-#
-##################################################################
-#   Использованные процедуры:
-# 1) average_by_group
-# 2) average_custom
-# 3)
-# 4)
-
+# 3) Нахождение лучщих студентов
 ###################################################################
 # Исходные данные
 ###################################################################
+import sys
 students = [
         {"Name":"Alex","Surname":"Ivanov","Gender":"M","Experienced":False,
          "Homeworks":[10,5,7,3,6],"Exam":8},
@@ -42,105 +36,124 @@ students = [
 
 
 ###################################################################
-# Функция average_by_group
+# Функция count_avg_by_person - подсчет среднего по отдельному студенту
 ###################################################################
-def average_by_group(students, option):
-    def count_avg_by_person(student):
-            avg_homework = 0
-            avg_exam = student["Exam"]
-            homework_marks = student["Homeworks"]
-            count_marks = 0
-            for mark in homework_marks:
-                count_marks += 1
-                avg_homework += mark
-            avg_homework /=count_marks
-            return avg_homework, avg_exam 
+def count_avg_by_person(student):
+        avg_homework = 0
+        exam = student["Exam"]
+        homework_marks = student["Homeworks"]
+        count_marks = 0
+        for mark in homework_marks:
+            count_marks += 1
+            avg_homework += mark
+        avg_homework /=count_marks
+        return avg_homework, exam
 
-
-    def coutn_by_group(students):
-        pass
     
-    if option == "G":
-        avg_homework_males = 0
-        avg_exam_males = 0
-        count_males = 0
-        avg_homework_females = 0
-        avg_exam_females = 0
-        count_females = 0
-        for student in students:
-            avg_homework_by_person, avg_exam_by_person = count_avg_by_person(student)
-            if student["Gender"] == "M":
-                avg_homework_males += avg_homework_by_person
-                avg_exam_males += avg_exam_by_person
-                count_males += 1
-            elif student["Gender"] == "F":
-                avg_homework_females += avg_homework_by_person
-                avg_exam_females += avg_exam_by_person
-                count_females += 1    
-        avg_homework_males /= count_males
-        avg_exam_males /= count_males
-        avg_homework_females /= count_females
-        avg_exam_females /= count_females
-        return avg_homework_males, avg_exam_males, avg_homework_females, avg_exam_females
-    
-    elif option == "E":
-        avg_homework_experienced = 0
-        avg_exam_experienced = 0
-        count_experienced = 0
-        avg_homework_unexperienced = 0
-        avg_exam_unexperienced = 0
-        count_unexperienced = 0
-        for student in students:
-            avg_homework_by_person, avg_exam_by_person = count_avg_by_person(student)
-            if student["Experienced"] == True:
-                avg_homework_experienced += avg_homework_by_person
-                avg_exam_experienced += avg_exam_by_person
-                count_experienced += 1
-            elif student["Unexperienced"] == False:
-                avg_homework_unexperienced += avg_homework_by_person
-                avg_exam_unexperienced += avg_exam_by_person
-                count_unexperienced += 1    
-        avg_homework_males /= count_males
-        avg_exam_males /= count_males
-        avg_homework_females /= count_females
-        avg_exam_females /= count_females
-        return avg_homework_males, avg_exam_males, avg_homework_females, avg_exam_females
+###################################################################
+# Функция count_statistic - основная функция подсчета статистики по
+# всей группе(с выборкой и без)
+###################################################################
+def count_statistic(students, option):
+    avg_group1_homework = 0
+    avg_group1_exam = 0
+    avg_group2_homework = 0
+    avg_group2_exam = 0
+    if option == "Gender":
+        avg_group1_homework, avg_group1_exam = avg_custom_group(students, option, "M") 
+        avg_group2_homework, avg_group2_exam = avg_custom_group(students, option, "F")
+        return avg_group1_homework, avg_group1_exam, avg_group2_homework, avg_group2_exam
+    elif option == "Experienced":
+        avg_group1_homework, avg_group1_exam = avg_custom_group(students, option, True) 
+        avg_group2_homework, avg_group2_exam = avg_custom_group(students, option, False)
+        return avg_group1_homework, avg_group1_exam, avg_group2_homework, avg_group2_exam
     else:
-        avg_homework_by_group = 0
-        avg_exam_by_group = 0
-        count_students = 0
-        for student in students:
+        avg_group1_homework, avg_group1_exam = avg_custom_group(students, option, None) 
+        return avg_group1_homework, avg_group1_exam
+
+
+###################################################################
+# Функция avg_custom_group - подсчет статистики относительно всей
+# группы по определенному признаку
+###################################################################
+def avg_custom_group(students, option, value):
+    avg_custom_group_homework = 0
+    avg_custom_group_exam = 0
+    count_custom_group = 0
+    for student in students:        
+        if student[option] == value or value == None:
             avg_homework_by_person, avg_exam_by_person = count_avg_by_person(student)
-            avg_homework_by_group += avg_homework_by_person
-            avg_exam_by_group += avg_exam_by_person
-            count_students+=1    
-        avg_homework_by_group/=count_students
-        avg_exam_by_group /= count_students
-        return avg_homework_by_group, avg_exam_by_group
-
-
-###################################################################
-# Функция customise_average
-###################################################################
-def customise_average():
-    option = input("Введите параметр выборки([G]ender / [E]xperienced):").upper()
-    if option == "G" | option == "E":
-        average_by_group(students, option)
-    else:
-        print("Введен неверный параметр. Повторите ввод.")
-        customise_average()
+            avg_custom_group_homework += avg_homework_by_person
+            avg_custom_group_exam += avg_exam_by_person
+            count_custom_group += 1
+    avg_custom_group_homework /= count_custom_group
+    avg_custom_group_exam /= count_custom_group
+    return avg_custom_group_homework, avg_custom_group_exam
 
 
 
 ###################################################################
-# Функция main
+# Функция find_best - поиск лучших студентов по всей группе
+###################################################################
+def find_best(students):
+    best_students = []
+    all_students = []
+    max_points = 0
+    for student in students:
+        homeworks, exam = count_avg_by_person(student)
+        points = 0.6 * homeworks + 0.4 * exam
+        if points > max_points:
+            max_points = points
+        all_students.append(points)
+    for index, student_points in enumerate(all_students):
+        if student_points == max_points:
+            best_students.append(students[index]["Name"] +" "+ students[all_students.index(student_points)]["Surname"])
+    print
+    return best_students, max_points
+
+###################################################################
+# Функция main - работа с пользователем и основная логика программы
 ###################################################################
 def main():
-    print("Средняя оценка за домашние задания: {}\nСредняя оценка за экзамен: {}".format(*average_by_group(students,"default")))
-    print("Средняя оценка за домашние задания у мужчин: {}\nСредняя оценка за экзамен у мужчин: {}\n\n\
-Средняя оценка за домашние задания у женщин: {}\nСредняя оценка за экзамен у женщин: {}".format(*average_by_group(students,"G")))
 
-main()
+
+    ###################################################################
+    # Функция input_option - подготовка условия выборки
+    ###################################################################
+    def input_option():
+        option = input("Выберите параметр ([П]ол, [О]пыт программирования):")
+        if option.upper() == "П":
+            print("Средняя оценка за домашние задания у мужчин: {}\nСредняя оценка за экзамен у мужчин: {}\n\n\
+Средняя оценка за домашние задания у женщин: {}\nСредняя оценка за экзамен у женщин: {}".format(*count_statistic(students, "Gender")))
+        elif option.upper() == "О":
+            print("Средняя оценка за домашние задания у студентов с опытом: {}\nСредняя оценка за экзамен у студентов с опытом: {}\n\n\
+Средняя оценка за домашние задания у студентов без опыта: {}\nСредняя оценка за экзамен у студентов без опыта: {}".format(*count_statistic(students, "Experienced")))
+        else:
+            print("Неверная команда. Повторите ввод.")
+            input_option()
+        
+    while True:
+        comand = input("Введите команду ([В]се студенты, [П]о признаку, [Л]учшие студенты, [З]акончить):")
+        if comand.upper() == "В":
+            print("Средняя оценка за домашние задания: {}\nСредняя оценка за экзамен: {}\n".format(*list(count_statistic(students,"Name"))))
+        elif comand.upper() == "П":
+            input_option()
+        elif comand.upper() == "Л": 
+            best_ones, max_points = find_best(students)
+            if len(best_ones) > 1:
+                print("Лучшие студенты: {0}. Максимальный балл: {1}".format(", ".join(best_ones), max_points))
+            else:
+                print("Лучший студент: {}. Максимальный балл: {}".format(best_ones[0], max_points ))
+        elif comand.upper() == "З":
+            print("Завершение работы. Нажмите любую клавишу")
+            input()
+            sys.exit()
+        else:
+            print("Неверная команда. Повторите ввод.")
+            main()
+
+if __name__ == '__main__':
+    main()
 
 
 
