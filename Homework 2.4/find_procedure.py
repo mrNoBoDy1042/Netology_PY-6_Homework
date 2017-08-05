97c6dbf6978da314a71f7f428e3fe3b374d3e821
mrNoBoDy <mrnobosy1042@ya.ru>
2017-08-05 21:34:35 +0300

    Исправлено: убраны вложеные функции, убран обработчик ошибок из-за ненадобности, добавлены док-стринги вместо комментариев. Добавлено: функция для нахождения пути. UPD: Исправлена почта в настройках клиента

--- Homework 2.4/find_procedure.py
+++ Homework 2.4/find_procedure.py
@@ -40,11 +40,17 @@
 import os
 
 
+def find_path():
+    """Находим путь до необходимых файлов
+    Возвращает путь"""
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, 'migrations')
+
+
 def find_all_files():
     """Находим папку Migrations и получаем список файлов.
     Возвращает список sql файлов."""
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    path_to_files = os.path.join(current_dir, 'migrations')
+    path_to_files = find_path()
     sql_files = list(filter(lambda x: x.endswith('.sql'), os.listdir(path_to_files)))
     return sql_files
 
@@ -58,8 +64,10 @@ def get_string():
 def find_string(user_file, user_string):
     """Работа с файлом. Чтение и поиск строки.
     Возвращает имя файла"""
+
     # Получаем содержимое файла
-    file_content = user_file.read()
+    with open(os.path.join(find_path(), user_file)) as f:
+        file_content = f.read()
 
     # Если в файле есть данная строка - возвращаем имя файла
     if user_string.lower() in file_content.lower():
@@ -71,12 +79,13 @@ def print_result(found_files):
     print()
     for item in found_files:
         print(item)
-    print('\nОбщее количество файлов: {}\nДля завершения поиска - CTRL + C'.format(found_files.count()))
+    print('\nКоличество найденных файлов: {}\nДля завершения поиска - CTRL + C'.format(len(found_files)))
     print('-'*50, end='\n\n')
 
 
 def find_matching_files(files_list):
     """Ищем все файлы с данной строкой"""
+
     # Получаем строку пользователя и ищем ее в файлах
     user_input = get_string()
     suitable_files = []
@@ -89,12 +98,15 @@ def find_matching_files(files_list):
 
     # Печать найденных результатов
     print_result(suitable_files)
+
     # Продолжение поиска среди найденных файлов
     find_matching_files(suitable_files)
 
 
 if __name__ == '__main__':
+
     # Получение списка файлов
     list_files = find_all_files()
+
     # Инициализация поиска среди файлов
     find_matching_files(list_files)
